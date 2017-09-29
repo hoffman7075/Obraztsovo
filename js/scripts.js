d3740a0c49139cfd56c5e4ae03d0dfd5932ab3b7
hoffman7075 <hoffman.ozr@gmail.com>
2017-09-29 09:01:27 +0500

    Учтены правки. Готово без js оптимизации

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,24 @@
 $(document).ready(function () {
+    // fixed menu
+    var headerHeight = $('header').innerHeight();
+    $('.banner:not(.footer)').css('padding-top',headerHeight);
+    $('.banner:not(.footer)').css('background-position','center '+ headerHeight+'px, center');
+
+    $( window ).resize(function() {
+        headerHeight = $('header').innerHeight();
+        $('.banner:not(.footer)').css('padding-top',headerHeight);
+        $('.banner:not(.footer)').css('background-position','center '+ headerHeight+'px, center');
+    });
+    
+    // open modal
+    $('.banner-button, .plan .plan-help .button').on('click', function () {
+        openModal($('.modal-form'));
+    })
+
+    $('.overlay, .close-modal').on('click',function () {
+        closeModal();
+    });
+
     // to second section
     $('.banner-button-down').on('click', function () {
         $('html, body').animate({
@@ -68,12 +88,90 @@ $(document).ready(function () {
         $('body').find('.mobile-menu').toggleClass('active');
         $('body').find('.overlay').toggleClass('active');
     })
-    
+
     // svg map
-    $('.fil5').on('click', function () {
+    $('.fil5').on('click', function (event) {
+        $('.fil5').removeClass('active');
+        $(this).addClass('active');
+        var x = event.pageX - $('.plan').offset().left;
+        var y = event.pageY - $('.plan').offset().top;
+        var popupHeight = $('.plan-popup').innerHeight();
+
+        // if popup ne vlazit v blok
+        if (y < ($('.kuchastku').offset().top - $('.plan').offset().top)/2) {
+            $('.plan').find('.plan-popup').css('top',y);
+        }
+        else {
+            $('.plan').find('.plan-popup').css('top',y-popupHeight);
+        }
+        $('.plan').find('.plan-popup').css('left',x);
         $('.plan').find('.plan-popup').addClass('active');
+
+        // add info to popup
+        $('.plan').find('.plan-popup .ploshad > span').html($(this).attr('sotki'));
+        $('.plan').find('.info span.green.price').html($(this).attr('cost')+' <small>рублей</small>');
+        $('.plan').find('.info span.green.sotka').html($(this).attr('weaving')+' рублей');
+        $('.plan').find('.info span.green.address').html($(this).attr('address'));
+        $('.plan').find('.plan-popup .plan-popup-wrapper p font').html($(this).attr('vygoda')+' <small>рублей</small>');
     })
     $('.plan .plan-popup .close').on('click', function () {
         $('.plan').find('.plan-popup').removeClass('active');
+        $('.fil5').removeClass('active');
     })
+
+    // scroll events
+    function scrollMeHarder(object,scrollStart,velocity,rotate) {
+        $(window).scroll(function(){
+            $(object).css('top',$('body').scrollTop()*velocity-scrollStart)
+            if (rotate!=0) {
+                $(object).css("-webkit-transform","rotate("+rotate*$('body').scrollTop()*0.01+"deg)");
+                $(object).css("-moz-transform","rotate("+rotate*$('body').scrollTop()*0.01+"deg)");
+                $(object).css("-ms-transform","rotate("+rotate*$('body').scrollTop()*0.01+"deg)");
+                $(object).css("-o-transform","rotate("+rotate*$('body').scrollTop()*0.01+"deg)");
+                $(object).css("transform","rotate("+rotate*$('body').scrollTop()*0.01+"deg)");
+            };
+        })
+    }
+
+    //ajax send email
+    $('form').submit(function(e) {
+        $.ajax({
+            type: "POST",
+            url: "mail.php",
+            data: $(this).serialize()
+        }).done(function() {
+            // $(this).fadeOut();
+            // $('.after-send').delay(400).fadeIn();
+            setTimeout(function () {
+                // Done Functions
+                $(this).trigger("reset");
+                //     $('.after-send').fadeOut();
+                //     $('#room-form, #contact-form').delay(400).fadeIn();
+            }, 1000);
+            alert('Сообщение отправлено!');
+        });
+        return false;
+    });
 })
+
+function openModal(element) {
+    $('.overlay').addClass('active');
+    element.css('display', 'block').animate({
+        opacity: 1,
+        top: '50%'
+    }, 200);
+}
+
+function closeModal() {
+    $('.modal')
+        .animate({
+                opacity: 0,
+                top: '45%'
+            }, 200,
+            function () {
+                $(this).css('display', 'none');
+                $('.overlay').removeClass('active');
+                $('.mobile-menu').removeClass('active');
+            }
+        );
+}
